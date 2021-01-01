1927c69c6a11074fd173d1118323097ea24a63d3
Zane-dev16 <imf.999kg@gmail.com>
2021-01-01 10:58:10 +0000

    Display movies

--- netflix-cln/src/App.js
+++ netflix-cln/src/App.js
@@ -5,7 +5,7 @@ import requests from './requests.js';
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <h1>Netflix</h1>
       <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals}/>
       <Row title="Trending Now" fetchUrl={requests.fetchTrending}/>
     </div >
--- netflix-cln/src/Row.js
+++ netflix-cln/src/Row.js
@@ -1,5 +1,8 @@
 import React, {useState, useEffect} from 'react'
 import axios from './axios'
+import './Row.css'
+
+const base_url = "https://image.tmdb.org/t/p/original/"
 
 function Row({ title, fetchUrl }) {
     const [movies, setMovies] = useState([])
@@ -17,9 +20,16 @@ function Row({ title, fetchUrl }) {
     console.log(movies)
 
     return (
-        <div>
+        <div className="row">
             <h2>{title}</h2>
-            {/* container -> posters */}
+
+            <div className="row_posters">
+                {movies.map(movie => (
+                    <img
+                        className="row_poster"
+                        src={`${base_url}${movie.poster_path}`} alt={movie.name} />
+                ))}
+            </div>
         </div>
     )
 }

7a25f194380041cee0a86aa953b7496e6cd13240
Zane-dev16 <imf.999kg@gmail.com>
2021-01-01 22:02:24 +0000

    Image styles and bug fixes

--- netflix-cln/src/Row.js
+++ netflix-cln/src/Row.js
@@ -4,7 +4,7 @@ import './Row.css'
 
 const base_url = "https://image.tmdb.org/t/p/original/"
 
-function Row({ title, fetchUrl }) {
+function Row({ title, fetchUrl, isLargeRow }) {
     const [movies, setMovies] = useState([])
 
     useEffect(() => {
@@ -15,9 +15,19 @@ function Row({ title, fetchUrl }) {
             return request
         }
         fetchData()
-    }, [fetchUrl])
+    }, [fetchUrl]);
 
-    console.log(movies)
+    function findImage(film) {
+        if (film.backdrop_path) {
+            return <img
+            key = {film.id}
+            className={`row_poster ${isLargeRow && "row_posterLarge"}`}
+            src={`${base_url}${isLargeRow ? film.poster_path : film.backdrop_path}`} 
+            alt={film.name} />
+        } else {
+            return
+        }
+    }
 
     return (
         <div className="row">
@@ -25,9 +35,7 @@ function Row({ title, fetchUrl }) {
 
             <div className="row_posters">
                 {movies.map(movie => (
-                    <img
-                        className="row_poster"
-                        src={`${base_url}${movie.poster_path}`} alt={movie.name} />
+                    findImage(movie)
                 ))}
             </div>
         </div>

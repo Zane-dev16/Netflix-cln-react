eba4089a34d65ed6639175794d97d78328ad659c
Zane-dev16 <imf.999kg@gmail.com>
2021-01-03 21:57:25 +0000

    Update Navigation bar

--- netflix-cln/src/Nav.js
+++ netflix-cln/src/Nav.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import './Nav.css'
 
 function Nav() {
     return (
         <div className="nav">
-            <img className="logo" src="https://i.imgur.com/x3JQ0FD.png" alt="Netflix Logo"/>
+            <img className="logo" src="https://1000logos.net/wp-content/uploads/2017/05/Netflix-Logo.png" alt="Netflix Logo"/>
+            <img className="icon" src="https://ih0.redbubble.net/image.618427277.3222/flat,1000x1000,075,f.u2.jpg" alt="Profile Icon"/>
         </div>
+        
     )
 }
 

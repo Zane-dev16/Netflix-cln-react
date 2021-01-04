f742c00a87ba5d5b6869b5b6429bd1ddf8f333a9
Zane-dev16 <imf.999kg@gmail.com>
2021-01-04 20:54:05 +0000

    Add trailer pop-ups and complete nav bar

--- netflix-cln/src/Nav.js
+++ netflix-cln/src/Nav.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import './Nav.css'
 
 function Nav() {
+    const [Show, setShow] = useState(false)
+
+    useEffect(() => {
+        window.addEventListener("scroll", () => {
+            if (window.scrollY > 100) {
+                setShow(true)
+            } else setShow(false)
+        })
+        return () => {
+            window.removeEventListener("scroll")
+        }
+    }, [])
+
     return (
-        <div className="nav">
+        <div className={`nav ${Show && 'black_nav'}`}>
             <img className="logo" src="https://1000logos.net/wp-content/uploads/2017/05/Netflix-Logo.png" alt="Netflix Logo"/>
             <img className="icon" src="https://ih0.redbubble.net/image.618427277.3222/flat,1000x1000,075,f.u2.jpg" alt="Profile Icon"/>
         </div>
--- netflix-cln/src/Row.js
+++ netflix-cln/src/Row.js
@@ -1,11 +1,14 @@
 import React, {useState, useEffect} from 'react'
 import axios from './axios'
 import './Row.css'
+import Youtube from  'react-youtube'
+import movieTrailer from "movie-trailer"
 
 const base_url = "https://image.tmdb.org/t/p/original/"
 
 function Row({ title, fetchUrl, isLargeRow }) {
     const [movies, setMovies] = useState([])
+    const [trailerUrl, setTrailerUrl] = useState('')
 
     useEffect(() => {
         async function fetchData() {
@@ -16,18 +19,42 @@ function Row({ title, fetchUrl, isLargeRow }) {
         fetchData()
     }, [fetchUrl]);
 
-    
+    const handleClick = (film) => {
+        if (trailerUrl) {
+            setTrailerUrl('')
+        } else {
+            try{
+                movieTrailer(film?.name)
+                .then((url) => {
+                    const urlParams = new URLSearchParams(new URL(url).search)
+                    setTrailerUrl(urlParams.get('v'))
+                })
+            }
+            catch(err) {
+                return
+            }
+        }
+    }
 
     function findImage(film) {
         if (film.backdrop_path) {
             return <img
             key = {film.id}
+            onClick={() => handleClick(film)}
             className={`row_poster ${isLargeRow && "row_posterLarge"}`}
             src={`${base_url}${isLargeRow ? film.poster_path : film.backdrop_path}`} 
             alt={film.name} />
         } else {
             return
         }
+    }
+
+    const opts = {
+        height: "390",
+        width: "100%",
+        payerVars: {
+            autoplay: 1,
+        },
     }
 
     return (
@@ -39,6 +66,7 @@ function Row({ title, fetchUrl, isLargeRow }) {
                     findImage(movie)
                 ))}
             </div>
+            {trailerUrl && <Youtube videoId={trailerUrl} opts={opts}/>}
         </div>
     )
 }
